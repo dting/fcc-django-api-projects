6e3c155600b427319e331b6cf9a1e066947999ee
DTing <dennistin.g@gmail.com>
2016-01-14 19:24:07 +0000

    Update settings.py allowed host for heroku

--- django_api_projects/settings.py
+++ django_api_projects/settings.py
@@ -17,15 +17,13 @@ import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-
 SECRET_KEY = os.environ.get('SECRET_KEY', 'application-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', True))
 
-ALLOWED_HOSTS = []
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
