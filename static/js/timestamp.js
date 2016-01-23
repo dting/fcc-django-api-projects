af246d33cd2d8523c8d3668330f14ba446a56041
DTing <dennistin.g@gmail.com>
2016-01-23 19:47:45 +0000

    Add timestamp button enable-disable support

--- static/js/timestamp.js
+++ static/js/timestamp.js
@@ -3,11 +3,14 @@
 $('#timestamp-form').submit(function(e) {
     e.preventDefault();
     e.stopPropagation();
+    $("#timestampSubmit").prop('disabled', true); // disable the button
     var jqxhr = $.getJSON('/api/timestamp/' + $('#timestamp-input').val());
     jqxhr.done(function(res) {
         $('#timestamp-output').html('<pre>' + JSON.stringify(res, null, 2) + '</pre>');
+        $("#timestampSubmit").prop('disabled', false); // enable the button
     });
     jqxhr.fail(function(jqxhr, textStatus, error) {
         $('#timestamp-output').html('<pre>' + textStatus + '</pre>');
+        $("#timestampSubmit").prop('disabled', false); // enable the button
     });
 });
