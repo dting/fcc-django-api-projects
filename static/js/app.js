d3720c8f42577a178fc4cacaeb9f623a0e415c9b
DTing <dennistin.g@gmail.com>
2016-01-24 06:07:16 +0000

    Add Javascript for linking to tab panels

--- static/js/app.js
+++ static/js/app.js
@@ -14,3 +14,15 @@ $.ajaxSetup({
         }
     }
 });
+
+// http://stackoverflow.com/questions/26352181/link-to-specific-tab-bootstrap
+// Javascript to enable link to tab
+var hash = document.location.hash;
+if (hash) {
+    $('.nav-tabs a[href="'+hash+'"]').tab('show');
+}
+
+// Change hash for page-reload
+$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+    window.location.hash = e.target.hash;
+});
